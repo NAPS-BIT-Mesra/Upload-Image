1cfa7036014016ae2fa4a1f539a634953fcd17db
NAPS-BIT-Mesra <naps@bitmesra.ac.in>
2022-01-22 20:04:01 +0530

    uuid added, error handling added

--- controller/imageController.js
+++ controller/imageController.js
@@ -1,39 +1,47 @@
-const multer = require("multer");
-const sharp = require("sharp");
-const multerStorage = multer.memoryStorage();
+const multer = require('multer')
+const sharp = require('sharp')
+const HttpError = require('../models/http-error')
+const { v1: uuid } = require('uuid')
+const multerStorage = multer.memoryStorage()
 const multerFilter = (req, file, cb) => {
-  if (file.mimetype.startsWith("image")) {
-    cb(null, true);
+  if (file.mimetype.startsWith('image')) {
+    cb(null, true)
   } else {
-    cb(new AppError("Not an image! Please upload only images.", 400), false);
+    cb(new AppError('Not an image! Please upload only images.', 400), false)
   }
-};
+}
 const upload = multer({
   storage: multerStorage,
-  fileFilter: multerFilter,
-});
-exports.uploadPhoto = upload.single("photo");
-exports.resizePhoto = async (req, res, next) => {
-  if (!req.file) return next();
-
-  req.file.filename = `editorial-${Date.now()}.jpeg`;
-
-  await sharp(req.file.buffer)
-    .resize(2000, 1333)
-    .toFormat("jpeg")
-    .jpeg({ quality: 90 })
-    .toFile(`public/img/${req.file.filename}`);
-
-  next();
-};
-exports.getImageURL = async (req, res, next) => {
+  fileFilter: multerFilter
+})
+const ResizePhoto = async (req, res, next) => {
+  if (!req.file) next(new HttpError('File not found!!', 404))
+  req.file.filename = `editorial-${uuid()}-${Date.now()}.jpeg`
+  try {
+    await sharp(req.file.buffer)
+      .resize(2000, 1333)
+      .toFormat('jpeg')
+      .jpeg({ quality: 90 })
+      .toFile(`public/img/${req.file.filename}`)
+  } catch (err) {
+    next(new HttpError('Error occurred while resizing image', 500))
+  }
+  next()
+}
+const GetImageURL = async (req, res, next) => {
   if (req.file.filename) {
     res.status(201).json({
-      status: "success",
-      message: "Image successfully Uploaded",
+      status: 'success',
+      message: 'Image successfully Uploaded',
       data: {
-        URL: `${req.protocol}://${req.get("host")}/img/${req.file.filename}`,
-      },
-    });
+        URL: `${req.protocol}://${req.get('host')}/img/${req.file.filename}`
+      }
+    })
+  } else {
+    next(new HttpError('File not found!!', 404))
   }
-};
+}
+
+exports.uploadPhoto = upload.single('photo')
+exports.getImageURL = GetImageURL
+exports.resizePhoto = ResizePhoto
